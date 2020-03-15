76c49650cab14ae5100210b97d4829450936c3e4
Morgan <caron.morgan91@gmail.com>
2020-03-15 16:39:21 +0100

    Minor fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
-'use strict'
+"use strict";
 
-const WebpackConfigGenerator = require('webpack-config-generator')
+const webpackConfigGenerator = require("webpack-config-generator");
 
 module.exports = (env, argv) => {
-	return WebpackConfigGenerator({
+	return webpackConfigGenerator({
 		mode: argv.mode,
 		entry: {
-			app: ['./src/docs/ts/App.ts', './src/docs/sass/style.sass']
+			app: ["./src/docs/ts/App.ts", "./src/docs/sass/style.sass"]
 		},
-		index: 'src/docs/index.html',
-		resourcesFolder: 'docs/',
-		favicon: './src/docs/favicon.png'
-	})
-}
+		index: "src/docs/index.html",
+		resourcesFolder: "docs/",
+		favicon: "./src/docs/favicon.png"
+	});
+};
